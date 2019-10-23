f325013016af394db80cbd7be2c5a77ce3ec3a27
Mason Louchart <mason@sekaijuu.de>
2019-10-23 20:09:48 +0900

    feat(connector): add Firebase connector
    This commit adds a fresh `hostabee-comment-flow-firebase-connector`
    (aka. Firebase connector) element to the collection. This one provide
    and easy way to link the `hostabee-comment-flow` to a Firebase app.
    Using this connector only few lines of code are required to setup a
    Firebase app and store comments into its Firestore.

---
+++ test/firebase-config.js
@@ -0,0 +1,10 @@
+// Put the configuration of the app you want to use for running test suites.
+// Required only for demo with Firebase connector.
+(function() {
+  window.HCF = window.HCF || {};
+  // HCF.firebaseConfig = {
+  //   apiKey: "AIza...",
+  //   authDomain: "YOU_APP.firebaseapp.com",
+  //   projectId: "YOU_APP",
+  // };
+})();
